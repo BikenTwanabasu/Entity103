1889f9ff1b44774029adcf33949d39d92a00dc8d
biken.twanabasu <biken.twanabasu@IME-BIKENTW>
2025-06-27 15:20:21 +0545

    Add project files.

---
+++ Entity103/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Entity103.Models
+{
+    public class Employee
+    {
+        public int EmployeeId {  get; set; }
+        public string EmployeeName { get; set; } = null!;
+        public string EmployeeAddress { get; set; } = null!;
+    }
+}
